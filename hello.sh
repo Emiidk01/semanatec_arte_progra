3c811df4f096b597a31ca41acb3ecab436a20a63
emi17 <a01710711@tec.mx>
2024-03-20 17:37:03 -0600

    Add new files to the repo

---
+++ hello.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo -e "\n"
+echo -e  "Hola $1, bienvenido al curso.\n"
+echo -e "El script en ejecucion es: $0\n"
+echo "El directorio actual es:"
+pwd
+echo -e "\n"
+echo -e "y su contenido es: \n"
+ls -la 
+echo -e "\n"
+
